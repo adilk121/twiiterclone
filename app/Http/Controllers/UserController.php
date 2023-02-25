fc10d9382290beb461cd9c4a9e01f71adb41b56d
Adil Khan <112151943+adilk121@users.noreply.github.com>
2023-02-25 22:25:47 +0530

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+// More comment
 class UserController extends Controller
 {
 
